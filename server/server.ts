07d2441954850fcb840cf36799264034427630aa
Joel Dias Correia <JoelCorreia@medife.com.ar>
2022-06-01 21:40:32 -0300

    react SRR Completed

--- server/server.ts
+++ server/server.ts
@@ -6,9 +6,6 @@ import ReactDOMServer from 'react-dom/server'
 import { App } from '../client/components/app'
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send('Hello from Server')
-})
 
 server.listen(3000, () => {
     console.log(`Server running on http://localhost:3000`)
